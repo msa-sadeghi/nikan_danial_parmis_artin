0805ec3cdc0b1678e2fcea27ebd3ff6b90635e1a
msa-sadeghi <sadeghi.msai@gmail.com>
2024-04-30 19:26:37 +0330

    a

--- pygame_course/second_game/main.py
+++ pygame_course/second_game/main.py
@@ -1,4 +1,10 @@
 import pygame
+from player import Player
+
+clock = pygame.time.Clock()
+
+p = Player(100, 300)
+
 
 screen = pygame.display.set_mode()
 running = True
@@ -7,4 +13,8 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+    screen.fill((0,0,0))
+    p.draw(screen)
+    p.animation()
     pygame.display.update()
+    clock.tick(60)
--- pygame_course/second_game/player.py
+++ pygame_course/second_game/player.py
@@ -2,4 +2,37 @@ from pygame.sprite import Sprite
 import pygame
 
 class Player(Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        
+        self.idle_images  = []
+        self.run_images  = []
+        
+        for i in range(5):
+            img = pygame.image.load(f"player/Idle/{i}.png")
+            img = pygame.transform.scale(img, (96,96))
+            self.idle_images.append(img)
+        
+        for i in range(6):
+            img = pygame.image.load(f"player/Run/{i}.png")
+            img = pygame.transform.scale(img, (96,96))
+            self.run_images.append(img)
+        self.image = self.idle_images[0]
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.image_number = 0
+        self.update_time = pygame.time.get_ticks()
     
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        
+    def animation(self):
+        self.image = self.run_images[self.image_number]
+        if pygame.time.get_ticks() - self.update_time > 200:
+            self.update_time = pygame.time.get_ticks()
+            self.image_number += 1
+            if self.image_number >= len(self.run_images):
+                self.image_number = 0
+            
+            
+        
