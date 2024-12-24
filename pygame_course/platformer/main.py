47e27ee1c04fb987f2318f90edcb449be7ae5655
msa-sadeghi <sadeghi.msai@gmail.com>
2024-12-24 18:55:46 +0330

    ن

--- pygame_course/platformer/main.py
+++ pygame_course/platformer/main.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.time import Clock
 from player import Player
+from world import World
+from level_creator import level_data
 pygame.init()
 
 screen_width = 800
@@ -13,6 +15,9 @@ c = Clock()
 sky_image = pygame.image.load("assets/sky.png")
 sky_rect = sky_image.get_rect() 
 sky_rect.topleft = (0,0)
+
+my_world = World(level_data)
+
 
 my_player = Player(300, 300)
 jumped = False
@@ -29,7 +34,8 @@ while running:
                 jumped = False
             
             
-    screen.blit(sky_image, sky_rect)   
+    screen.blit(sky_image, sky_rect) 
+    my_world.draw(screen)  
     if jumped == True:
         my_player.vel_y = -13
     my_player.gravity()
--- pygame_course/platformer/world.py
+++ pygame_course/platformer/world.py
@@ -1,4 +1,18 @@
+import pygame
+
+
 class World:
     def __init__(self, data):
-        self.world_data = data
-        
+        dirt_image = pygame.image.load("assets/dirt.png")
+        dirt_image = pygame.transform.scale(dirt_image, (50,50))
+        self.world_data = []
+        for i in range(len(data)):
+            for j in range(len(data[i])):
+                if data[i][j] == 1:
+                    rect = dirt_image.get_rect(topleft=(j * 50, i * 50))
+                    self.world_data.append((dirt_image, rect))
+                    
+    def draw(self, screen):
+        for tile in self.world_data:
+            screen.blit(tile[0], tile[1])
+            
